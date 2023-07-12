fe296356b6cc24257051e2cae4b8836181039c9a
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2023-07-12 03:14:05 -0400

    added everything needed for a deployment to vercel with django/ postgresql

--- nyc_subway_beherenow/settings.py
+++ nyc_subway_beherenow/settings.py
@@ -15,6 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Vercel paths
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -25,7 +30,9 @@ SECRET_KEY = 'django-insecure-1&2mzwi*^h%r1(upx+v8+z4jbyl12d5yoxh=l#4r&n@a@^9584
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1','.now.sh']
 
 
 # Application definition
@@ -43,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,16 +83,28 @@ WSGI_APPLICATION = 'nyc_subway_beherenow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nyc_subway_beherenow',
-        'USER': 'nyc_subway_beherenow_admin',
-        'PASSWORD': 'password',
-        'HOST': 'localhost'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'nyc_subway_beherenow',
+#         'USER': 'nyc_subway_beherenow_admin',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost'
+#     }
+# }
+import dj_database_url
+\
+import environ  
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+ 
+DATABASE_URL=env('DATABASE_URL')
 
+DATABASES = {
+    'default': 
+        dj_database_url.config('DATABASE_URL')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- nyc_subway_beherenow/wsgi.py
+++ nyc_subway_beherenow/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nyc_subway_beherenow.settings')
 
 application = get_wsgi_application()
+app = application
+
