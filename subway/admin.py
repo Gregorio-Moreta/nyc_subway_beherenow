72a0f07bf98899c51219732bae82f3fcfdcbb0e3
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2023-10-05 09:01:31 -0400

    only the readme so you can build along yourselves

--- subway/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Train, Line, Station
-
-admin.site.register(Train)
-admin.site.register(Line)
-admin.site.register(Station)
